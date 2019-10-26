95b3f1f3b29372bf6a932a879a5f97763ea8852c
rourke750 <rourke7501@gmail.com>
2019-10-26 17:59:10 -0400

    more changes
    add token authentication
    add aditional login method
    Start using tokens for authentication.
    Accept image uploading

--- hackgtserver/project/server.py
+++ hackgtserver/project/server.py
@@ -1,20 +1,41 @@
-from flask import Flask, request, json
+from flask import Flask, request, json, make_response, jsonify
 from . import util, database as db
 
 import asyncio
 
 api = Flask(__name__)
 
-@api.route('/user/create', methods=['POST'])
+@api.route('/user/create', methods=['GET'])
 def create_username():
     username = request.args.get('username', type = str)
     password = request.args.get('password', type = str)
     hashed = util.generate_first_hashed(password)
     success = db.create_user(username, password)
     if success:
-        return json.dumps({"success": True}), 201
+        token = util.encode_auth_token(username)
+        responseObject = {
+            'success': True,
+            'token': token.decode()
+        }
+        return make_response(jsonify(responseObject)), 201
     else:
         return json.dumps({"success": False}), 500
+  
+@api.route('/user/login', methods=['GET'])  
+def log_user_in():
+    username = request.args.get('username', type = str)
+    password = request.args.get('password', type = str)
+    hashed = db.get_user_password(username)
+    success = util.check_password(password, hashed)
+    if success:
+        token = util.encode_auth_token(username)
+        responseObject = {
+            'success': True,
+            'token': token.decode()
+        }
+        return make_response(jsonify(responseObject)), 200
+    else:
+        return json.dumps({"success": False}), 401
 
 @api.route('/user/exist', methods=['POST'])
 def is_username_exists():
@@ -26,6 +47,33 @@ def is_username_exists():
         return json.dumps({"success": False}), 500
     else:
         return json.dumps({"success": False}), 200
+        
+@api.route('/img/upload', methods=['POST'])
+def upload_image():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        auth_token = ''
+    if auth_token:
+        resp = util.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            user = User.query.filter_by(id=resp).first()
+            responseObject = {
+                'success': True
+            }
+            return make_response(jsonify(responseObject)), 200
+        responseObject = {
+            'success': False,
+            'message': resp
+        }
+        return make_response(jsonify(responseObject)), 401
+    else:
+        responseObject = {
+                'success': False,
+                'message': 'Provide a valid auth token.'
+            }
+        return make_response(jsonify(responseObject)), 401
 
 if __name__ == '__main__':
     api.run()
--- hackgtserver/project/util.py
+++ hackgtserver/project/util.py
@@ -1,4 +1,8 @@
 import bcrypt
+import jwt
+import datetime
+
+SECRET_KEY = os.getenv('PROJECT_HACKGT_SECRET_KEY', 'key_not_ransom')
 
 def generate_first_hashed(password):
     """
@@ -8,3 +12,37 @@ def generate_first_hashed(password):
     
 def check_password(plain_text_password, hashed_password):
     return bcrypt.checkpw(plain_text_password, hashed_password)
+    
+def encode_auth_token(user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=0),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            SECRET_KEY,
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+        
+@staticmethod
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, SECRET_KEY)
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
